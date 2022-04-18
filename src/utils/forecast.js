60f6f8fdd4b31321b820568d9ec922f456a6bce4
Rishav Srivastava <rishavsrivastava2005@gmail.com>
2022-04-18 22:40:46 +0530

    Changed temperature to celsius

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request=require('request');
 
 const forecast=(latitude,longitude,callback)=>{
-    const url=`http://api.weatherstack.com/current?access_key=965e1794338bf54e89e737cc16b8ce62&query=${latitude},${longitude}&units=f`;
+    const url=`http://api.weatherstack.com/current?access_key=965e1794338bf54e89e737cc16b8ce62&query=${latitude},${longitude}`;
     
     // request({url: url, json:true},(error,response)=>{//old code without destructuring the response object and without directly using url variable which has same variable name as it's property
     request({url, json:true},(error,{ body }={})=>{//destructuing body from response object
